35ad633e5a6368451cdb614864067856af052ecd
Saurabh Tiwari <techsaurabh.tiwari@gmail.com>
2026-01-21 18:05:25 +0530

    Session 1: Variables and Data Types

---
+++ homework_session1.py
@@ -0,0 +1,26 @@
+name = "Saurabh"
+age = 21
+city = "Chennai"
+height = 5.6
+is_developer = True
+favourite_language = ["Python", "Javascript"]
+
+
+print("Name :", name)
+print("Age :", age)
+print("City :", city)
+print("Height :", height)
+print("Is Developer :", is_developer)
+print("Favourite Programming Language :", favourite_language)
+
+
+print("\n Data Types")
+print(type(name))
+print(type(age))
+print(type(city))
+print(type(height))
+print(type(is_developer))
+print(type(favourite_language))
+
+
+
